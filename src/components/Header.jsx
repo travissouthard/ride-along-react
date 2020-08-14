6a804e479ee72e6e5211684efb266616b5dc26c9
Travis Southard <tsouthard88@gmail.com>
2020-08-14 13:28:16 -0400

    Started to style the header just to help the look a little bit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <header>
-            <img src="" alt="Logo"/>
+            <img id="logo" src="../images/RideAlongTransparentWhite.png" alt="Logo"/>
             <h1>Ride Along Pictures, Ltd.</h1>
         </header>
     )
