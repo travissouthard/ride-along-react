db351f653d0058026a92c82a0874bc0504e09bac
Travis Southard <tsouthard88@gmail.com>
2020-08-17 23:01:01 -0400

    Added more styling to the static pages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,12 +5,12 @@ export default function Contact() {
         <main>
             <h2>Contact Us</h2>
             <ul>
-                <li>YouTube</li>
-                <li>Email</li>
-                <li>Twitter</li>
-                <li>Instagram</li>
+                <a href="https://www.youtube.com/channel/UC6P4_LQ-Y9QPVarkHEnCbEw" target="new"><li>YouTube</li></a>
+                <a href="mailto:ridealongpictures@gmail.com"><li>Email</li></a>
+                <a href="https://twitter.com/ridealongpics" target="new"><li>Twitter</li></a>
+                <a href="https://instagram.com/ridealongpictures" target="new"><li>Instagram</li></a>
             </ul>
-            <p>You can hire us to help you design a custom adventure, email us for details!</p>
+            <p>You can hire us to help you design a custom adventure,  <a href="mailto:ridealongpictures@gmail.com">email us</a> for details!</p>
         </main>
     )
 }
