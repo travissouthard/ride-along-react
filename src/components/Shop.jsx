9cca86b9b81e14bb4684a6c15e8f544e6fe8c26b
Travis Southard <tsouthard88@gmail.com>
2020-08-19 01:37:59 -0400

    Finalized the responsive styling

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -34,60 +34,58 @@ export default function Shop(props) {
     return (
         <main>
             <h2>Ride Along Shop</h2>
-            <div className="cart">
-                {props.checkoutOn ? (
-                    <div>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Item</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                    <th>Delete</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {props.cart < 0 ? "" : (
-                                    props.cart.map((item, index) => {
-                                        return <tr key={index}>
-                                            <td>{item.name}</td>
-                                            <td>{item.quantity}</td>
-                                            <td>{"$" + item.price.toFixed(2)}</td>
-                                            <td onClick={() => props.removeItem(index)}>X</td>
-                                        </tr>
-                                    })
-                                )}
-                                <tr>
-                                    <td></td>
-                                    <td>Subtotal</td>
-                                    <td>{"$" + props.amount.toFixed(2)}</td>
-                                    <td onClick={() => {props.resetCart()}}>X</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <form className="checkout" onSubmit={(event) => handlePaymentSubmit(event)}>
-                            <label htmlFor="customerName"> Full name: 
-                                <input type="text" id="customerName" name="customerName" value={props.customerName} onChange={(event) => props.handleChange(event, props.setCustomerName)}/>
-                            </label>
-                            <label htmlFor="email"> Email: 
-                                <input type="email" id="email" name="email" value={props.email} onChange={(event) => props.handleChange(event, props.setEmail)}/>
-                            </label>
-                            <label htmlFor="amount"> Amount (US$): 
-                                <input type="text" id="amount" name="amount" disabled value={props.amount.toFixed(2)}/>
-                            </label>
-                            <CardElement onChange={(event) => handleCardChange(event)}/>
-                            <input type="submit" value="Checkout" disabled/>
-                            <button onClick={() => props.toggleCheckout()}>Cancel</button>
-                        </form>
-                    </div>
-                ) : (
-                    <div>
-                        <h3>{props.cart.length + " items in your cart!"}</h3>
-                        <button onClick={() => props.toggleCheckout()}>Begin checkout</button>
-                        <button onClick={() => props.resetCart()}>Empty Cart</button>
-                    </div>
-                )}
-            </div>
+            {props.checkoutOn ? (
+                <div className="cart">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.cart < 0 ? "" : (
+                                props.cart.map((item, index) => {
+                                    return <tr key={index}>
+                                        <td>{item.name}</td>
+                                        <td>{item.quantity}</td>
+                                        <td>{"$" + item.price.toFixed(2)}</td>
+                                        <td onClick={() => props.removeItem(index)}>X</td>
+                                    </tr>
+                                })
+                            )}
+                            <tr>
+                                <td></td>
+                                <td>Subtotal</td>
+                                <td>{"$" + props.amount.toFixed(2)}</td>
+                                <td onClick={() => {props.resetCart()}}>X</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <form className="checkout" onSubmit={(event) => handlePaymentSubmit(event)}>
+                        <label htmlFor="customerName"> Full name: 
+                            <input type="text" id="customerName" name="customerName" value={props.customerName} onChange={(event) => props.handleChange(event, props.setCustomerName)}/>
+                        </label>
+                        <label htmlFor="email"> Email: 
+                            <input type="email" id="email" name="email" value={props.email} onChange={(event) => props.handleChange(event, props.setEmail)}/>
+                        </label>
+                        <label htmlFor="amount"> Amount (US$): 
+                            <input type="text" id="amount" name="amount" disabled value={props.amount.toFixed(2)}/>
+                        </label>
+                        <CardElement onChange={(event) => handleCardChange(event)}/>
+                        <input type="submit" value="Checkout" disabled/>
+                        <button onClick={() => props.toggleCheckout()}>Cancel</button>
+                    </form>
+                </div>
+            ) : (
+                <div>
+                    <h3>{props.cart.length + " items in your cart!"}</h3>
+                    <button onClick={() => props.toggleCheckout()}>Begin checkout</button>
+                    <button onClick={() => props.resetCart()}>Empty Cart</button>
+                </div>
+            )}
             <div className="shop">
                 {props.products.length < 1 ? (
                     <p>New products coming soon!</p>
